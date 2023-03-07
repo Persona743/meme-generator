ab21b6eb0eaed0da32e245c28fbe5038a5136ab2
Persona743 <siyavashshahidi7@gmail.com>
2023-03-07 22:55:11 +0330

    input added

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -36,7 +36,11 @@ export default function Meme() {
                     Get a new meme image 😎
                 </button>
             </div>
-            <img src={meme.randomImage} className="meme--image" />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">One does not simply</h2>
+                <h2 className="meme--text bottom">Walk into Mordor</h2>
+            </div>
         </main>
     );
 }
